9f9d76a87924cf7fc2c0d534150d598a9a1d3c34
eams1798 <eams1798@gmail.com>
2022-11-01 00:22:38 -0400

    now the translation packages are loaded successfully

--- src/config/i18next-config.js
+++ src/config/i18next-config.js
@@ -1,12 +1,16 @@
 import { initReactI18next } from 'react-i18next';
 import i18next from 'i18next';
-import i18nBackend from 'i18next-http-backend';
+import Backend from 'i18next-http-backend';
 
 i18next
-  .use(i18nBackend)
+  .use(Backend)
   .use(initReactI18next)
   .init({
     interpolation: {escapeValue: false},
     lng: 'en',
     fallbackLng: 'en',
+    backend: {
+      // for all available options read the backend's repository readme file
+      loadPath: '/world-cup-score-simulator/locales/{{lng}}/{{ns}}.json'
+    }
   });
